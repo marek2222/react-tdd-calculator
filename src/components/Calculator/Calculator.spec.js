055102a20c0eecfa5c1c705cca305bcdfd275c31
marek2222 <marekmusial2@o2.pl>
2018-12-19 10:20:53 +0100

    react-tdd-calculator: 6_2:  Calculator Component - Calculator Shallow Rendering Test

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,3 +1,12 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
+import { Calculator } from './Calculator';
 
+describe('Calculator', () => {
+    let wrapper;
+    beforeEach(() => wrapper = shallow(<Calculator />));
+
+    if('should render a <div />', () => {
+        expect(wrapper.find('div').lenght).toEqual(1);
+    });
+});
