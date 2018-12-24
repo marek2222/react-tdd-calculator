c8b1b3957faebfaff8e936955bf35a598e3305ef
marek2222 <maekmusial2@o2.pl>
2018-12-24 16:19:54 +0100

    react-tdd-calculator: 9_2:  Key Component - Add Key CSS

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Key = ({ keyAction, keyType, keyValue }) => 
-        <div className="key-container" />;
+const Key = ({ keyAction, keyType, keyValue }) => (
+    <div className="key-container">
+        <p className="key-value">
+            {keyValue}
+        </p>
+    </div>
+);
 
 Key.propTypes = {
     keyAction: PropTypes.func.isRequired, 
